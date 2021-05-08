d270ced61f8664af02b23270f265da4906817865
kp425 <krishna.kp07@gmail.com>
2021-05-08 18:32:22 +0000

    Testing the initial setup

---
+++ build.sh
@@ -0,0 +1,36 @@
+
+function get_env_files() {
+    
+    remote_url=$1
+    env_folder=$2
+
+    echo $remote_url
+    echo $env_folder
+
+    current="$(dirname "$BASH_SOURCE")"
+    echo $current
+
+    tmp_folder="$current/temp"
+    echo $tmp_folder
+    mkdir $tmp_folder
+    git clone $remote_url $tmp_folder
+
+    # env_folder="BinanceBotEnvs"
+    
+    src="$tmp_folder/$env_folder/."
+    dest="$current/env"
+
+    echo $src
+    echo $dest
+
+    cp -a $src $dest
+    rm -r "$current/temp"
+}
+
+
+get_env_files $1 $2
+
+current="$(dirname "$BASH_SOURCE")"
+pip install -r "$current/requirements.txt"
+
+
