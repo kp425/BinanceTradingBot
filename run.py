6e4ab15195fb5ba681f7d15e1b6b8792fe504e31
kp425 <krishna.kp07@gmail.com>
2021-05-09 11:11:46 +0000

    Testing the initial setup

--- run.py
+++ run.py
@@ -1,5 +1,37 @@
-from settings import API_KEY, SECRET_KEY
+from settings import config
+import requests
+import asyncio
+import hashlib
+import hmac
 
-print(API_KEY)
-print(SECRET_KEY)
+API_KEY = config.API_KEY
+SECRET_KEY = config.SECRET_KEY
+API_URL = config.API
 
+
+def get_signature():
+    hashedsig = hashlib.sha256(SECRET_KEY)
+
+
+def get_headers():
+        headers = {
+            'Accept': 'application/json',
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',  # noqa
+        }
+        if config.API_KEY:
+            assert API_KEY
+            headers['X-MBX-APIKEY'] = API_KEY
+        return headers
+
+url = API_URL + "/v3/account"
+print(url)
+resp = requests.get(url, headers=get_headers(),
+                params=dict(symbol="ETHBUSD"))
+print(resp.json())
+
+# r = requests.get("https://api.binance.com/api/v3/depth",
+#                  params=dict(symbol="ETHBUSD"))
+# results = r.json()
+
+# print(r)
+# print(results)
--- settings.py
+++ settings.py
@@ -2,8 +2,34 @@ import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 
-dotenv_path = os.path.join(dirname(__file__), 'env/dev.env')
-load_dotenv(dotenv_path)
 
-API_KEY = os.environ.get("API_KEY")
-SECRET_KEY = os.environ.get("SECRET_KEY")
+dev_config = {
+
+}
+
+class LoadConfig:
+    def __init__(self, env_path):
+        self.env_path = env_path 
+        load_dotenv(env_path)
+        self.API_KEY = os.environ.get("API_KEY")
+        self.SECRET_KEY = os.environ.get("SECRET_KEY")
+        self.API = os.environ.get("API")
+        self.WS = os.environ.get("WS")
+        self.STREAM = os.environ.get("STREAM")
+
+dev = True 
+
+if dev:
+    dev_path = os.path.join(dirname(__file__), 'env/dev.env')
+    config = LoadConfig(dev_path)
+else:
+    prod_path = os.path.join(dirname(__file__), 'env/prod.env')
+    config = LoadConfig(prod_path)
+
+
+
+
+
+
+
+
