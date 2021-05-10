f40e5b58d4e68e3bac0097cb3cebfbd1a4903332
kp425 <krishna.kp07@gmail.com>
2021-05-10 12:37:24 +0000

    Shell trading working

--- sample.sh
+++ sample.sh
@@ -2,23 +2,26 @@
 
 timestamp=$(date +%s%N)
 timestamp=${timestamp:0:13}
-echo $timestamp
+
 
 SECRET=$2
 BASE_URL="https://testnet.binance.vision"
 API_PATH="/api/v3/order"
 
-# QUERY_STRING="symbol=LTCBTC&side=BUY&type=LIMIT&timeInForce=GTC&quantity=1&price=0.1&recvWindow=5000&timestamp=1499827319559"
-QUERY_STRING="symbol=BNBUSDT&side=BUY&type=LIMIT&timeInForce=GTC&quantity=1&price=20&recWindow=20000&timestamp=$timestamp"
 
-echo $QUERY_STRING
+# QUERY_STRING="symbol=BNBUSDT&side=BUY&type=LIMIT&timeInForce=GTC&quantity=1&price=20&recWindow=20000&timestamp=$timestamp"
+
+QUERY_STRING="symbol=BNBUSDT&side=BUY&type=LIMIT&quantity=1&timeInForce=GTC&price=400&timestamp=$timestamp"
+
 hash=$(echo -n $QUERY_STRING | \
 openssl dgst -sha256 -hmac $SECRET)
 
 hash=$(echo $hash | sed 's/(stdin)= //g')
 
-
 URL="$BASE_URL$API_PATH?$QUERY_STRING&signature=$hash"
+
+echo $timestamp
+echo $QUERY_STRING
 echo $URL
 echo $hash
 
