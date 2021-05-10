c28889713af2cc3f168521cee0c73e2630ff0677
kp425 <krishna.kp07@gmail.com>
2021-05-10 10:47:07 +0000

    Added stream functionality

--- sample.sh
+++ sample.sh
@@ -1,10 +1,21 @@
+#!/usr/bin/env bash
+
+SECRET=$2
+BASE_URL="https://testnet.binance.vision"
+API_PATH="/api/v3/order"
+
+# QUERY_STRING="symbol=LTCBTC&side=BUY&type=LIMIT&timeInForce=GTC&quantity=1&price=0.1&recvWindow=5000&timestamp=1499827319559"
+QUERY_STRING="symbol=BNBUSDT&side=BUY&type=LIMIT&timeInForce=GTC&quantity=1&price=20&recWindow=20000&timestamp=1620641702892"
+
+echo $QUERY_STRING
+hash=$(echo -n $QUERY_STRING | \
+openssl dgst -sha256 -hmac $SECRET)
+
+hash=$(echo $hash | sed 's/(stdin)= //g')
 
 
-echo "$1"
-echo $2
-echo $3
-echo $4
+URL="$BASE_URL$API_PATH?$QUERY_STRING&signature=$hash"
+echo $URL
+echo $hash
 
-echo symbol=BNBUSDT&side=BUY&type=LIMIT&timeInForce=GTC&quantity=1&price=20&recWindow=20000&timestamp=1620597003781 | openssl dgst -sha256 -hmac $3
-
-curl -H "X-MBX-APIKEY: $2" -X POST $4
+curl -H "X-MBX-APIKEY: $2" -X POST $URL
